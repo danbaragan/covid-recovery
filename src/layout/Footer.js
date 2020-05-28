cbc385ed71a5ba4586228fbea4277a89754bf271
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-05-28 10:20:37 +0300

    style: Make footer smaller

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className="sm:sticky bottom-0 container layout-shell rounded-b-lg pb-10">
+    <div className="sm:sticky bottom-0 container layout-shell rounded-b-lg">
       <div className="sm:flex">
-        <p className="w-5/6 text-center mb-6" >3BgsbUzdYVgB4oLyiyFDwY1oyhxUVj3iub</p>
+        <p className="w-5/6 text-center mb-4" >3BgsbUzdYVgB4oLyiyFDwY1oyhxUVj3iub</p>
         <div className="flex">
-          <Link className="flex-1 link text-center text-xl font-bold" to="/">Recovery</Link>
-          <Link className="flex-1 link text-center text-xl font-bold" to="/about">About</Link>
+          <Link className="flex-1 link text-center text-lg font-bold" to="/">Recovery</Link>
+          <Link className="flex-1 link text-center text-lg font-bold" to="/about">About</Link>
         </div>
       </div>
     </div>
